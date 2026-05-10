776a351d482800e85034b97c621f65e5faf8855e
TRMashedelf <trmasshedelf@gmail.com>
2026-05-10 14:27:04 +0800

    Smoothed turning + Sprint

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 2.0
+const SPRINT_SPEED = 5.0
+const TURN_SPEED = 5.0         
+const SPRINT_TURN_SPEED = 10.0 
 
 @export var camera: Camera3D = null
 
@@ -11,6 +13,10 @@ func _process(_delta) -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	var sprinting := Input.is_action_pressed("sprint")
+	var current_speed := SPRINT_SPEED if sprinting else SPEED
+	var current_turn_speed := SPRINT_TURN_SPEED if sprinting else TURN_SPEED
 
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 
@@ -25,14 +31,14 @@ func _physics_process(delta: float) -> void:
 
 		var direction := (right * input_dir.x + forward * input_dir.y).normalized()
 
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 
-		var look_target := global_position + direction
-		look_at(look_target, Vector3.UP)
-
+		var target_angle := atan2(-direction.x, -direction.z)
+		var current_angle := rotation.y
+		rotation.y = lerp_angle(current_angle, target_angle, current_turn_speed * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
