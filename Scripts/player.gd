5e13feac474e830b0a1256fbe44e8e1e24fb0fb3
TRMashedelf <trmasshedelf@gmail.com>
2026-05-11 19:21:17 +0800

    Fixed player import - Broken animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -57,7 +57,9 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("inventory"):
 		HUD.visible = !HUD.visible
-		get_tree().paused = !get_tree().paused
+	elif event.is_action_pressed("escape"):
+		var pause_menu = HUD.PauseMenu
+		pause_menu.visible = !pause_menu.visible
 
 func _physics_process(delta: float) -> void:
 	if get_tree().paused:
